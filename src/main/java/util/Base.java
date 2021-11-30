d1b59a270ee1d7c4534cf53ef93f130f8e97f1ac
Norbert D <deak.norbert@outlook.com>
2021-11-30 17:46:15 +0200

    added base files

---
+++ src/main/java/util/Base.java
@@ -0,0 +1,150 @@
+package util;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.JavascriptExecutor;
+import java.util.concurrent.TimeUnit;
+import java.io.IOException;
+import java.io.File;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    private JavascriptExecutor js;
+
+    public JavascriptExecutor getJS() {
+        js = (JavascriptExecutor) driver;
+        return js;
+    }
+
+    public WebDriver initializeDriver() throws IOException {
+        WebDriverManager.chromedriver().setup();
+        driver = WebDriverFactory.createWebDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().deleteAllCookies();
+        return driver;
+    }
+
+    public void waitForElementToBeVisible(By by) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+
+    public void waitForElementToBeVisible(By by, int timeout) {
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        } catch (Exception e) {
+
+        }
+    }
+
+    public WebElement waitForElementToBeVisible(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void waitForElementToBeClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void waitForElementToBeSelected(WebElement element) {
+        wait.until(ExpectedConditions.elementSelectionStateToBe(element, true));
+    }
+
+    public void waitForElementToBeRefreshed(WebElement element) {
+        wait.until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(element)));
+    }
+
+    public void waitForElementToBeRefreshed(By by) {
+        wait.until(ExpectedConditions.refreshed(ExpectedConditions.elementToBeClickable(by)));
+    }
+
+    public void waitForElementToBeClickable(By by) {
+        wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
+
+    public WebElement waitForPresenceOfElement(By by) {
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    public void waitForPresenceOfAllElements(By by) {
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
+    }
+
+    public boolean waitForTextToBePresentInElement(By by, String text) {
+        try {
+            if (wait.until(ExpectedConditions.textToBePresentInElementLocated(by, text)))
+                return true;
+        } catch (TimeoutException e) {
+            return false;
+        }
+        return false;
+    }
+
+    public boolean waitForTextToBeNonEmpty(final By by) {
+        (new WebDriverWait(driver, 30)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.findElement(by).getText().length() != 0;
+            }
+        });
+        return false;
+    }
+
+    public void waitForInvisibilityOfElementByText(By by, String text) {
+        wait.until(ExpectedConditions.invisibilityOfElementWithText(by, text));
+    }
+
+    public void waitForElementToBeInvisible(By by) {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+
+    public void waitForElementToBeChecked(WebElement element) {
+        wait.until(elementToBeChecked(element));
+    }
+
+    private static ExpectedCondition<WebElement> elementToBeChecked(
+            final WebElement element) {
+        return new ExpectedCondition<WebElement>() {
+
+            public ExpectedCondition<WebElement> visibilityOfElement =
+                    ExpectedConditions.visibilityOf(element);
+
+            @Override
+            public WebElement apply(WebDriver driver) {
+                WebElement element = visibilityOfElement.apply(driver);
+                try {
+                    if (element != null && element.getAttribute("checked").equals("true")) {
+                        return element;
+                    } else {
+                        return null;
+                    }
+                } catch (StaleElementReferenceException e) {
+                    return null;
+                }
+            }
+
+            @Override
+            public String toString() {
+                return "element to be checked : " + element;
+            }
+        };
+    }
+
+    public String getScreenShotPath(String testCaseName, WebDriver driver) throws IOException {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        String destinationFile = System.getProperty("user.dir") + "//reports//" + testCaseName + ".png";
+        FileUtils.copyFile(source, new File(destinationFile));
+        return destinationFile;
+    }
+}
