dd3ba11f3a02199fe6ca19267c049fb51d1733c7
Norbert D <deak.norbert@outlook.com>
2021-11-30 17:44:51 +0200

    first commit

---
+++ src/main/java/util/WebDriverFactory.java
@@ -0,0 +1,26 @@
+package util;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+    public static WebDriver createWebDriver() {
+        String webdriver = System.getProperty("browser", "chrome");
+        switch(webdriver) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+            case "safari":
+                //TODO: implement SafariDriver to factory
+                //return new SafariDriver();
+                //throw new RuntimeException("Unsupported webdriver: " + webdriver);
+            default:
+                throw new RuntimeException("Unsupported webdriver: " + webdriver);
+        }
+    }
+}
